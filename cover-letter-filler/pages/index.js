7a3d9589c3f1bf523aba96a3db8b67be7f9fd062
Jacky Chen <jacky.chen0319@gmail.com>
2021-08-16 15:25:51 -0700

    Updated styling

--- cover-letter-filler/pages/index.js
+++ cover-letter-filler/pages/index.js
@@ -14,8 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Cover Letter Filler
-        </h1>
-        <p>Get started by importing your cover letter!</p>
+        </h1><br/>
       <Form/>
       </main>
 
