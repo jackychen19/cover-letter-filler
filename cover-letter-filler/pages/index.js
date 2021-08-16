e0b4c56107420a1524d0898eb43281617bc2f2fd
Jacky Chen <jacky.chen0319@gmail.com>
2021-08-16 14:57:50 -0700

    Uploaded sample cover letter

---
+++ cover-letter-filler/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import Form from '../components/Form.js';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cover Letter Filler</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Cover Letter Filler
+        </h1>
+        <p>Get started by importing your cover letter!</p>
+      <Form/>
+      </main>
+
+
+    </div>
+  )
+}
