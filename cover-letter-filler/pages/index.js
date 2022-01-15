f21683e64176048b523eb79d3ea04fc12eb0211b
Jacky Chen <jacky.chen0319@gmail.com>
2022-01-14 16:34:05 -0800

    Added more material.ui stying

--- cover-letter-filler/pages/index.js
+++ cover-letter-filler/pages/index.js
@@ -5,17 +5,17 @@ import Form from '../components/Form.js';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Cover Letter Filler</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>"/>
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1 className={styles.title}>
           Cover Letter Filler
-        </h1><br/>
-      <Form/>
+        </h1>
+        <Form/>
       </main>
 
 
